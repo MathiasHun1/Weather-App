4fed2ecf98642c08686d074002c4ebbe2d32c94c
MathiasHun <mathias.hun@gmail.com>
2024-04-03 20:40:11 +0200

    .

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -2,7 +2,7 @@ import { returnIconName } from "../conditions"
 
 const get3daysForecast = async (town) => {
     //fetch data and format it for easier usage in the app
-    let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=fceeec4c1b054e0e95084438240204&q=${town}&days=3&aqi=no&alerts=no`, {
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=fceeec4c1b054e0e95084438240204&q=${town}&days=3&aqi=no&alerts=no`, {
         mode: 'cors'
     })
     let result = await response.json()
